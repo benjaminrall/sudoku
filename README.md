789a176577e8b50765430c1fe88f0cbe85c1356e
benjaminrall <benr2508@aol.com>
2023-10-09 15:27:30 +0100

    Command line arguments for main + Readme

---
+++ README.md
@@ -0,0 +1,111 @@
+# Python Sudoku
+
+This project provides the ability to solve and generate sudoku puzzles using an efficient implementation of the Dancing Links method, as presented by Donald Knuth in [this paper](https://arxiv.org/abs/cs/0011047).
+
+There are 3 ways to interact with the scripts in the repository:
+- Solving sudokus with `solver.py`
+- Generating sudokus with `generator.py`
+- Playing sudokus with `main.py`
+
+## Solving Sudokus
+
+To solve any sudoku, it can be passed as a command line argument when running the `solver.py` script. The sudoku must be formatted as a sudoku string, with 81 digits from 0-9 in a string (where 0 represents empty cells). The string should be created by reading the cells of a sudoku puzzle row by row from left to right and then top to bottom. Empty cells can also be represented by a '.'.
+
+By default, the result will be another sudoku string in the same form as the input. A second optional command line argument can be specified to change the printing style - 0 (default) will result in a sudoku string, and 1 will result in a pretty printed grid, making the solution easily readable.
+
+#### Examples
+```bash
+>>> py solver.py 000000010400000000020000000000050407008000300001090000300400200050100000000806000
+693784512487512936125963874932651487568247391741398625319475268856129743274836159
+```
+
+```bash
+>>> py solver.py 000801000000000043700000000000050800020030000000000100600000075003400000000200600 1
+235|841|796
+186|597|243
+794|326|518
+---+---+---
+417|652|839
+528|139|467
+369|784|152
+---+---+---
+642|918|375
+873|465|921
+951|273|684
+```
+
+## Generating Sudokus
+
+Random sudokus can be generated using the `generator.py` script. No command line arguments are required, but an optional display mode argument can be provided which works identically to `solver.py`.
+
+Sudokus generated tend to have between 20 and 28 clues, but this can vary. The sudokus are not graded in any way, and can vary hugely in difficulty since they are just completely random. However, it is guaranteed that all sudokus will be minimal (removing any more clues will result in more than 1 possible solution).
+
+#### Examples
+
+```bash
+>>> py generator.py
+004100008010034050030000000000060705000400000069870000203000000001950307000002800
+```
+
+```bash
+>>> py generator.py 1
+  5|  8|  9
+79 |  4|   
+  6|   |7
+---+---+---
+   |  9|  8
+ 53|   | 1
+ 4 |   |  6
+---+---+---
+   | 87|
+   | 53| 21
+9 2|1  | 5
+```
+
+## Playing Sudokus
+
+Randomly generated sudokus can be played through a graphical interface created using Pygame. Command line arguments for running the `main.py` script are as follows:
+
+```
+usage: main.py [-h] [-s SUDOKU] [-a {0,1}] [-d DIMENSIONS] [-f FRAMERATE]
+
+options:
+  -h, --help            show this help message and exit
+  -s SUDOKU, --sudoku SUDOKU
+                        sudoku string to initialise the program with
+  -a {0,1}, --appearance {0,1}
+                        the appearance of the program, 0 is light mode and 1 is dark mode
+  -d DIMENSIONS, --dimensions DIMENSIONS
+                        the size of the square display window in pixels
+  -f FRAMERATE, --framerate FRAMERATE
+                        the maximum framerate of the program
+```
+
+When run without any of the optional arguments, a random sudoku grid is automatically generated and displayed. The default appearance is light mode, default display size is 800 pixels, and default framerate is 60.
+
+To use the sudoku grid, you can simply click on a cell in the grid to select it and then type a number (1-9) to enter it into the cell. Backspace or Delete can be pressed to remove the contents of the selected cell, and Space can be used to clear all cells on the grid. The starting cells are locked, and are not affected by any of these methods.
+
+When a cell is selected, the selection can be moved around the grid using the arrow keys. Pressing the Z key allows any changes made to be undone.
+
+When an incorrect value is entered, the cell is highlighted in red. There is no limit to the amount of incorrect values that can be edited, but for the best experience solving the sudoku should still be attempted with as few mistakes as possible.
+
+When the complete solution is entered, congratulations text will appear along with the time that it took to complete the puzzle. You can start a new puzzle by pressing the R key to generate a new random sudoku.
+
+#### Examples
+
+```bash
+py main.py -s 000801000000000043700000000000050800020030000000000100600000075003400000000200600
+```
+
+![Example 1](/imgs/example_1.png)
+
+```bash
+py main.py -a 1
+```
+
+![Example 2](/imgs/example_2.png)
+
+#### Gameplay Examples
+
+![Example 3](/imgs/example_3.png)
+![Example 4](/imgs/example_4.png)
--- main.py
+++ main.py
@@ -10,36 +10,86 @@ from solver import SudokuSolver
 from board import SudokuBoard
 from generator import SudokuGenerator
 
-# Constants
-WIN_SIZE = 800
-FRAMERATE = 60
 ICON_IMG = pygame.image.load(os.path.join("imgs", "icon.png"))
-
-TILE_SIZE = WIN_SIZE // 15
-BOARD_SIZE = 9 * (TILE_SIZE + 2)
-BOARD_SCREEN_POS = (WIN_SIZE - BOARD_SIZE) // 2
-BOARD_POS = (BOARD_SCREEN_POS, BOARD_SCREEN_POS)
+MINIMUM_DIMENSIONS = 200
+DEFAULT_DIMENSIONS = 800
+MINIMUM_FRAMERATE = 24
+DEFAULT_FRAMERATE = 60
 
 # Methods
-def create_new_sudoku(board: SudokuBoard) -> tuple[np.ndarray, np.ndarray]:
-    """Returns a new sudoku and its solution."""
-    sudoku = SudokuGenerator.generate_puzzle()
+def set_sudoku(board: SudokuBoard, sudoku=None) -> tuple[np.ndarray, np.ndarray]:
+    """Sets the sudoku for the board or generates a new puzzle."""
+    if sudoku is None:
+        sudoku = SudokuGenerator.generate_puzzle()
     solution = SudokuSolver.solve(sudoku.copy())
     board.set_sudoku(sudoku)
     return sudoku, solution
 
+def sudoku_argument(arg: str) -> np.ndarray | None:
+    if arg is None:
+        return arg
+
+    # Gets the sudoku string from command line arguments
+    sudoku_string = arg.replace('.', '0')
+
+    # Ensures sudoku string is of the correct format
+    if len(sudoku_string) != 81 or not sudoku_string.isnumeric():
+        raise argparse.ArgumentTypeError(
+            "sudoku puzzle must be represented as a string of 81 digits from 0-9")
+
+    # Converts string input to a numpy array containing the sudoku
+    sudoku = np.array(list(sudoku_string), dtype=int).reshape((9, 9))
+
+    # Ensures the sudoku only has one solution
+    if SudokuSolver.count_solutions(sudoku, 2) != 1:
+        raise argparse.ArgumentTypeError(
+            "sudoku puzzle must have a single solution to be valid")
+
+    return sudoku
+
+def dimensions_argument(arg: str) -> int:
+    value = int(arg)
+    if value < MINIMUM_DIMENSIONS:
+        raise argparse.ArgumentTypeError(
+            f"value must be greater than {MINIMUM_DIMENSIONS}")
+    return value
+
+def framerate_argument(arg: str) -> int:
+    value = int(arg)
+    if value < MINIMUM_FRAMERATE:
+        raise argparse.ArgumentTypeError(
+            f"value must be greater than {MINIMUM_FRAMERATE}")
+    return value
+
 # Main Loop
 if __name__ == '__main__':
-    """
-    Args:
+    parser = argparse.ArgumentParser()
 
-    sudoku (str): sudoku string to initialise the program with. default None.
-    appearance (int): 0 for dark mode, 1 for light mode. default 0.
-    
-    """
+    # Adds command line arguments to be parsed
+    parser.add_argument("-s", "--sudoku", type=sudoku_argument, default=None, 
+        help="sudoku string to initialise the program with")
+    parser.add_argument("-a", "--appearance", type=int, choices=[0, 1], default=0,
+        help="the appearance of the program, 0 is light mode and 1 is dark mode")
+    parser.add_argument("-d", "--dimensions", type=dimensions_argument, default=DEFAULT_DIMENSIONS, 
+        help="the size of the square display window in pixels")
+    parser.add_argument("-f", "--framerate", type=framerate_argument, default=DEFAULT_FRAMERATE,
+        help="the maximum framerate of the program")
 
-    # Pygame Setup
-    window = pygame.display.set_mode((WIN_SIZE, WIN_SIZE))
+    # Parses the command line arguments
+    args = parser.parse_args()
+    sudoku = args.sudoku
+    appearance = [SudokuBoard.LIGHT_MODE, SudokuBoard.DARK_MODE][args.appearance]
+    win_size = args.dimensions
+    framerate = args.framerate
+
+    # Calculates values for the board's tile size and position
+    tile_size = win_size // 15
+    board_size = 9 * (tile_size + 2)
+    board_screen_pos = (win_size - board_size) // 2
+    board_pos = (board_screen_pos, board_screen_pos)
+
+    # Pygame setup
+    window = pygame.display.set_mode((win_size, win_size))
     pygame.display.set_caption("Sudoku")
     pygame.display.set_icon(ICON_IMG)
     clock = pygame.time.Clock()
@@ -47,12 +97,12 @@ if __name__ == '__main__':
     # Creates font to be displayed when you win
     if not pygame.font.get_init():
         pygame.font.init()
-    win_text_font = pygame.font.SysFont("", int(TILE_SIZE * 0.8))
+    win_text_font = pygame.font.SysFont("", int(tile_size * 0.8))
 
     # Sets up the board and creates the starting sudoku
     start_time = time.time()
-    board = SudokuBoard(TILE_SIZE, BOARD_POS, SudokuBoard.DARK_MODE)
-    sudoku, solution = create_new_sudoku(board)
+    board = SudokuBoard(tile_size, board_pos, appearance)
+    sudoku, solution = set_sudoku(board, sudoku)
 
     # Initialises loop variables
     running = True
@@ -60,7 +110,7 @@ if __name__ == '__main__':
 
     # Main game loop
     while running:
-        clock.tick(FRAMERATE)
+        clock.tick(framerate)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 # Ensures safe exiting from the application
@@ -79,7 +129,7 @@ if __name__ == '__main__':
                 if event.key == pygame.K_r:
                     # Resets the sudoku when the 'R' key is pressed
                     over = False
-                    sudoku, solution = create_new_sudoku(board)
+                    sudoku, solution = set_sudoku(board)
                 elif event.key == pygame.K_SPACE and not over:
                     # Allows the board to be cleared using the space key
                     board.clear()
@@ -107,7 +157,8 @@ if __name__ == '__main__':
 
         # Detects if the game is over
         over = board.check_solution()
-        if over: board.deselect_all()
+        if over: 
+            board.deselect_all()
         
         # Update the display after everything has been drawn
         pygame.display.update()
